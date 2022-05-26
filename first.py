637d76748568315d386b19650f2de28fb12c7f07
Sleepiii <95644163+Onlyizy@users.noreply.github.com>
2022-05-25 22:46:14 -0400

    Update first.py

--- first.py
+++ first.py
@@ -3,13 +3,39 @@ import numpy as np
 import os
 from datetime import datetime
 
-log=open("log.txt","a")
-time=datetime.now
+VID_FILETYPES=[".mp4",".mov",".avi",".flv",".mkv",".wmv","h264",".264","mpeg"]
 
-log.writelines(datetime)
+def finder()->bool:
+  log=open("log.txt","a")
+  time=datetime.now
+  log.write(datetime)
+  path=input("Copy the path to the directory and paste it here: ")
+  lsdir=os.list.dir(path)
+  log.write(path)
+  log.writelines(lsdir)
+  for file in lsdir:
+    if file[::-1][:-3][::-1].lower() in VID_FILETYPES:
+      
+      
+  
 
-video=cv2.VideoCapture("")
+def trimmer()->bool:
+  
 
-print(log.read())
 
-log.close()
+def logwriter()->bool:
+  
+
+
+
+
+
+
+  video=cv2.VideoCapture("")
+
+  print(log.read())
+
+  log.close()
+
+
+if __name__==__main__:
