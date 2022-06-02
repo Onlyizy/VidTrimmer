6ea95ac6a10a1e6afcccc9ee62354336ba1d53a2
Onlyizy <kmorelisaac@gmail.com>
2022-06-02 19:31:54 -0400

    Signed-off-by: Onlyizy <kmorelisaac@gmail.com>

--- first.py
+++ first.py
@@ -7,38 +7,46 @@ from datetime import datetime
 
 VID_FILETYPES=[".mp4",".mov",".avi",".flv",".mkv",".wmv","h264",".264","mpeg"]
 
-def finder()->bool:
-  log=open("log.txt","a")
-  time=datetime.now
-  log.write(datetime)
-  path=input("Copy the path to the directory and paste it here: ")
-  lsdir=os.list.dir(path)
-  log.write(path)
-  log.writelines(lsdir)
-  for file in lsdir:
-    if file[::-1][:-3][::-1].lower() in VID_FILETYPES:
-      
-      
+def finder(path:str,timeset:int)->int:
+  counter=0
+  lsdir=os.listdir(path)
+  #for file in lsdir:
+    #if file[::-1][:-3][::-1].lower() in VID_FILETYPES:
+      #check if file is more than timeset seconds
+      #if  time_of_video<=timeset:
+        #pass
+      #else:
+        #trim
+        #counter+=1
+  logwriter(path)
+  return counter
+
+def usrprompt()->tuple:
+  while True:
+    path=input("Copy the path to the directory and paste it here: ")
+    timeset=int(input("Please enter the number of seconds you would like the videos to be trimmed to. We recommend either 30s or 10s for social media content: "))
+    finder(path,timeset)
+
       
   
 
 def trimmer()->bool:
-  
-
-
-def logwriter()->bool:
-  
-
-
-
-
-
-
   video=cv2.VideoCapture("")
+  
 
+
+def logwriter(path:str)->bool:
+  log=open("log.txt","a+")
+  time=datetime.now()
+  log.write(str(time))
+  log.write("\n")
+  lsdir=os.listdir(path)
+  log.write(path)
+  log.write("\n")
+  log.writelines(lsdir)
   print(log.read())
-
   log.close()
 
+if __name__=="__main__":
+  usrprompt()
 
-if __name__==__main__:
