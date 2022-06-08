8a9114ae2a4a7b6ee96ccbd92f9d3dd6fef7965c
Sleepiii <95644163+Onlyizy@users.noreply.github.com>
2022-06-08 19:46:12 -0400

    Update first.py

--- first.py
+++ first.py
@@ -8,33 +8,32 @@ from moviepy.editor import *
 
 VID_FILETYPES=[".mp4",".mov",".avi",".flv",".mkv",".wmv","h264",".264","mpeg"]
 
-def finder(path:str,timeset:int)->int:
+def findandTrim(path:str,timeset:int)->int:
   counter=0
+  newclips=[]
+  cuttracker={}
   lsdir=os.listdir(path)
-  #for file in lsdir:
-    #if file[::-1][:-3][::-1].lower() in VID_FILETYPES:
-      #check if file is more than timeset seconds
-      #if  time_of_video<=timeset:
-        #pass
-      #else:
-        #trim
-        #counter+=1
-  logwriter(path)
+  for file in lsdir:
+    if file[::-1][:-3][::-1].lower() in VID_FILETYPES:
+      clipcount=0
+      video=VideoFileClip(file)
+      duration=float(clip.duration)
+      if duration>timeset:
+        for start in range(duration.ceil())[::timeset]:
+          newclips.append(video.subclip(start,start+timeset))
+          clipcount=+1
+        cuttracker.setdefault(file,clipcount)
+     counter=+1
+  logwriter(path,counter,cuttracker)
   return counter
 
-def usrprompt()->tuple:
+def usrprompt():
   while True:
     path=input("Copy the path to the directory and paste it here: ")
     timeset=int(input("Please enter the number of seconds you would like the videos to be trimmed to. We recommend either 30s or 10s for social media content: "))
     finder(path,timeset)
   
-
-def trimmer()->bool:
-  video=cv2.VideoCapture("")
-  
-
-
-def logwriter(path:str)->bool:
+def logwriter(path:str,)->bool:
   log=open("log.txt","a+")
   time=datetime.now()
   log.write(str(time))
