4d23da51bf1355679ae8e7035a7d41eeef476d8e
Onlyizy <kmorelisaac@gmail.com>
2022-06-08 20:19:56 -0400

    Finally I have somthing working

--- first.py
+++ first.py
@@ -13,17 +13,21 @@ def findandTrim(path:str,timeset:int)->int:
   newclips=[]
   cuttracker={}
   lsdir=os.listdir(path)
+  print(lsdir)
   for file in lsdir:
-    if file[::-1][:-3][::-1].lower() in VID_FILETYPES:
-      clipcount=0
-      video=VideoFileClip(file)
-      duration=float(clip.duration)
-      if duration>timeset:
-        for start in range(duration.ceil())[::timeset]:
-          newclips.append(video.subclip(start,start+timeset))
-          clipcount=+1
-        cuttracker.setdefault(file,clipcount)
-     counter=+1
+    for ext in VID_FILETYPES:
+      if file.endswith(ext):
+        print()
+        print(file)
+        clipcount=0
+        video=VideoFileClip(path+"\\"+file)
+        duration=float(video.duration)
+        if duration>timeset:
+          for start in range(int(duration))[::timeset]:
+            newclips.append(video.subclip(start,start+timeset))
+            clipcount=+1
+          cuttracker.setdefault(file,clipcount)
+        counter=+1
   logwriter(path,counter,cuttracker)
   return counter
 
@@ -31,9 +35,9 @@ def usrprompt():
   while True:
     path=input("Copy the path to the directory and paste it here: ")
     timeset=int(input("Please enter the number of seconds you would like the videos to be trimmed to. We recommend either 30s or 10s for social media content: "))
-    finder(path,timeset)
+    findandTrim(path,timeset)
   
-def logwriter(path:str,)->bool:
+def logwriter(path:str,counter,cuttracker)->bool:
   log=open("log.txt","a+")
   time=datetime.now()
   log.write(str(time))
